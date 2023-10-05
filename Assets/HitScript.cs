707aa708d901a67268c854b84611a07db1bcc472
Maarten Gielkens <maarten.gielkens@student.uhasselt.be>
2023-10-05 12:22:59 +0200

    target score

--- Assets/HitScript.cs
+++ Assets/HitScript.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class HitScript : MonoBehaviour
 {
     private int hitCounter = 0;
     [SerializeField]
-    private GameObject bulletPrefab;
+    private TMP_Text scoreText;
+    [SerializeField]
+    private Transform targetCenter;
 
     // Start is called before the first frame update
     void Start()
@@ -24,11 +29,33 @@ public class HitScript : MonoBehaviour
     {
         if (collision.gameObject.tag == "Bullet")
         {
+            Vector3 colissionPoint = collision.contacts[0].point;
+            float xDist = (colissionPoint.x - targetCenter.position.x);
+            float yDist = (colissionPoint.y - targetCenter.position.y);
+            float dist = Mathf.Sqrt(xDist * xDist + yDist * yDist);
+            Debug.Log("dist is " + dist  );
+            double score = Math.Ceiling(10 - ((1 + ((10 - 1) / (0.3 - 0)) * (dist - 0))));
+
+            Debug.Log("score is " + score);
+            //Debug.Log(targetCenter);
+            //float distanceToCollision = Vector3.Distance(targetCenter.position, colissionPoint);
+            //Debug.Log(distanceToCollision);
+            //int score = CalculateScore(distanceToCollision);
+
             Debug.Log("Entered collision nfuewhfewuhfuiewhfuiew");
+
+
+            scoreText.text = score.ToString();
         }
         
     }
 
+    int CalculateScore(float distance)
+    {
+        float distancePerCircle = distance / 10;
+        int score = Mathf.FloorToInt(distance / distancePerCircle);
+        return score;
+    }
     /*
     private void OnCollisionEnter(Collision collision)
     {
