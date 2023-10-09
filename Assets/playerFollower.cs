563030f1a31a6cafde52fa06b5002467817c4c25
SimonK-2055442 <simon_knuts@student.uhasselt.be>
2023-10-09 17:14:47 +0200

    menu reset

--- Assets/playerFollower.cs
+++ Assets/playerFollower.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class playerFollower : MonoBehaviour
+public class PlayerFollower : MonoBehaviour
 {
 
     [SerializeField]
     private GameObject canvas;
+    [SerializeField]
+    private Transform player;
     private bool active = false;
 
     void Start()
@@ -17,12 +20,19 @@ public class playerFollower : MonoBehaviour
     private void Update()
     {
         if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
-        {
-            Debug.Log("werkt");
+        { 
             if (active == false)
             {
                 active = true;
                 canvas.SetActive(true);
+
+                Vector3 playerPosition = player.position;
+                Quaternion playerRotation = player.rotation;
+                Vector3 playerForward = player.forward;
+
+                Vector3 newPosition = playerPosition + playerForward * 0.5f;
+                canvas.GetComponent<Transform>().position = newPosition;
+                canvas.GetComponent<Transform>().rotation = playerRotation;
             }
             else
             {
@@ -30,5 +40,11 @@ public class playerFollower : MonoBehaviour
                 canvas.SetActive(false);
             }
         }
+    }
+
+    public void OnResetSelect()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
 }
